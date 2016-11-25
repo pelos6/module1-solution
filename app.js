67f83b1286ffe0ba4f002595e6ed819c504b49fb
pelos6 <javieriranzo@hotmail.com>
2016-11-25 23:56:00 +0100

    primer commit

---
+++ app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+
+// .controller('LunchCheckController', function ($scope) {
+  $scope.lunchMenu = "";
+  $scope.mensaje = "";
+  $scope.Check = function () {
+    var separator = ",";
+    var stringToSplit = $scope.lunchMenu;
+    var arrayOfStrings = stringToSplit.split(separator) ;
+    if (stringToSplit  ) {
+      if (arrayOfStrings.length < 4 ) {
+        $scope.mensaje = "Enjoy!" + arrayOfStrings.length;
+      } else {
+        $scope.mensaje = "Too much!" + arrayOfStrings.length;
+      }
+    } else {
+      $scope.mensaje = "Please enter data first!" ;
+    }
+  };
+};
+
+
+})();
